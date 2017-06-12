393785f3d64e7ce2b5f90849182fab2ced3d2f40
CamiloUgis <caasalin@alumnos.ubiobio.cl>
2017-06-12 15:08:14 -0400

    Relleno y división de linea rápido

--- www/js/index.js
+++ www/js/index.js
@@ -12,7 +12,11 @@ var mainView = myApp.addView('.view-main', {
 
 document.addEventListener("deviceready", function () {
     $('#geo').bind('click', geo);
-    $('#rapido').bind('change', lineaRapido);
+    $('#rapido1').bind('change', lineaRapido);
+    $('#rapido2').bind('change', lineaRapido);
+    $('#rapido3').bind('change', lineaRapido);
+    $('#rapido4').bind('change', lineaRapido);
+
     $('#linea13').bind('change', lineaTrece);
 });
 
@@ -56,7 +60,6 @@ function geo() {
                 }
 
                 //Línea rápido
-                if (mostrarRapido == true) {
 
                     //una dirección
                     var pointRapidoA = new google.maps.LatLng(-36.59853514532111, -72.10611864924431);
@@ -70,33 +73,69 @@ function geo() {
                     var pointRapidoOtherD = new google.maps.LatLng(-36.60246278338412, -72.09026545286179);
                     var pointRapidoOtherE = new google.maps.LatLng(-36.5976867147853, -72.10915893316269);
                     var pointRapidoOtherF = new google.maps.LatLng(-36.603922676817554, -72.11179822683334);
+
+                                    
                     var pointRapidoOtherG = new google.maps.LatLng(-36.60595097847018, -72.10399568080902);
                     var pointRapidoOtherH = new google.maps.LatLng(-36.60860362370549, -72.09332585334778);
-                    var pointRapidoOtherI = new google.maps.LatLng(-36.6097748925655, -72.0937067270279);
-                    var pointRapidoOtherJ = new google.maps.LatLng(-36.60833664081416, -72.09974706172943);
-                    var pointRapidoOtherK = new google.maps.LatLng(-36.61584625852837, -72.10259020328522);
-                    var pointRapidoOtherL = new google.maps.LatLng(-36.617327427046746, -72.09632456302643);
-                    var pointRapidoOtherM = new google.maps.LatLng(-36.61937690400007, -72.09514439105987);
+                    //variante dirección
+                    var pointRapido2A = new google.maps.LatLng(-36.60977273951406, -72.09364369511604);
+                    var pointRapido2B = new google.maps.LatLng(-36.608291425878136, -72.09976449608803);
+                    var pointRapido2C = new google.maps.LatLng(-36.61580535376147, -72.10259690880775);
+                    var pointRapido2D = new google.maps.LatLng(-36.61730589863834, -72.09636077284813);
+                    var pointRapido2E = new google.maps.LatLng(-36.61939843183017, -72.0951484143734);
+                    var pointRapido2F = new google.maps.LatLng(-36.623755540902636, -72.09791645407677);
+                    var pointRapido2G = new google.maps.LatLng(-36.62434321512527, -72.09399774670601);
+                    var pointRapido2H = new google.maps.LatLng(-36.626033022632164, -72.09490835666656);
+
+                    //variante dirección
+                    var pointRapido3A = new google.maps.LatLng(-36.62846111428764, -72.08824172616005);
+                    var pointRapido3B = new google.maps.LatLng(-36.62766467421417, -72.08750680088997);
+                    var pointRapido3C = new google.maps.LatLng(-36.628316894668366, -72.08619520068169);
+                    var pointRapido3D = new google.maps.LatLng(-36.628157607014025, -72.08531811833382);
+                    var pointRapido3E = new google.maps.LatLng(-36.627496774986106, -72.08577610552311);
+                    var pointRapido3F = new google.maps.LatLng(-36.627054961487765, -72.08333579823375);
+                    var pointRapido3G = new google.maps.LatLng(-36.625389931466685, -72.08317654207349);
+                    var pointRapido3H = new google.maps.LatLng(-36.625600349334015, -72.0695386826992);
+                    var pointRapido3I = new google.maps.LatLng(-36.62595552908568, -72.06791795790195);
                     
-                    
-                    
+                    if(mostrarRapido1==true){
                     requestDirections(pointRapidoA, pointRapidoB);
                     requestDirections(pointRapidoB, pointRapidoC);
                     requestDirections(pointRapidoC, pointRapidoD);
-
                     requestDirections(pointRapidoOtherA, pointRapidoOtherB);
                     requestDirections(pointRapidoOtherB, pointRapidoOtherC);
                     requestDirections(pointRapidoOtherC, pointRapidoOtherD);
                     requestDirections(pointRapidoOtherD, pointRapidoOtherE);
                     requestDirections(pointRapidoOtherE, pointRapidoOtherF);
                     requestDirections(pointRapidoOtherF, pointRapidoOtherG);
+                    }
+                    if(mostrarRapido2==true){
+                    
                     requestDirections(pointRapidoOtherG, pointRapidoOtherH);
-                    requestDirections(pointRapidoOtherH, pointRapidoOtherI);
-                    requestDirections(pointRapidoOtherI, pointRapidoOtherJ);
-                    requestDirections(pointRapidoOtherJ, pointRapidoOtherK);
-                    requestDirections(pointRapidoOtherK, pointRapidoOtherL);
-                    requestDirections(pointRapidoOtherL, pointRapidoOtherM);
-                }
+                    requestDirections(pointRapidoOtherH, pointRapido2A);
+                    requestDirections(pointRapido2A, pointRapido2B);
+                    requestDirections(pointRapido2B, pointRapido2C);
+                    requestDirections(pointRapido2C, pointRapido2D);
+                    requestDirections(pointRapido2D, pointRapido2E);
+                    requestDirections(pointRapido2E, pointRapido2F);
+                    requestDirections(pointRapido2F, pointRapido2G);
+                    requestDirections(pointRapido2G, pointRapido2H);
+                    }
+                    if(mostrarRapido3==true){
+                    requestDirections(pointRapido2H, pointRapido3A);
+                    requestDirections(pointRapido3A, pointRapido3B);
+                    requestDirections(pointRapido3B, pointRapido3C);
+                    requestDirections(pointRapido3C, pointRapido3D);
+                    requestDirections(pointRapido3D, pointRapido3E);
+                    requestDirections(pointRapido3E, pointRapido3F);
+                    requestDirections(pointRapido3F, pointRapido3G);
+                    requestDirections(pointRapido3G, pointRapido3H);
+                    requestDirections(pointRapido3H, pointRapido3I);                    
+                    }
+                    
+                    
+                    
+                    //requestDirections(pointRapido2E, pointRapidoOtherM);
 
                 if (mostrarRapido == true) {
 
@@ -120,13 +159,32 @@ function geo() {
 }
 
 function lineaRapido() {
-    if (document.getElementById('rapido').checked) {
-        mostrarRapido = true;
+    if (document.getElementById('rapido1').checked) {
+        mostrarRapido1=true;
+        mostrarRapido2=false;
+        mostrarRapido3=false;
         geo();
     } else {
-        mostrarRapido = false;
-        geo();
+        if (document.getElementById('rapido2').checked) {
+            mostrarRapido2=true;
+            mostrarRapido1=false;
+            mostrarRapido3=false;
+            geo();
+        }else{
+            if(document.getElementById('rapido3').checked){
+                mostrarRapido3=true;
+                mostrarRapido2=false;
+                mostrarRapido1=false;
+                geo();
+        }else{
+            mostrarRapido1 = false;
+            mostrarRapido2=false;
+            mostrarRapido3=false;
+            geo();
+        }
+       
     }
+}
 }
 
 function lineaTrece() {
